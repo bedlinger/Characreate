a00c7634018829bc5433c1b37ae0230172dccd87
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2025-01-16 15:33:34 +0100

    Fix styling of card

--- components/PersonaCard.vue
+++ components/PersonaCard.vue
@@ -3,30 +3,28 @@
     <template #title>{{ persona.name }} - {{ persona.occupation }}</template>
     <template #content>
       <div class="grid grid-cols-3 gap-6">
-        <div id="profile-picture">
-          <div class="relative">
-            <div
-              class="absolute inset-0 bg-gradient-to-t from-slate-100 to-transparent"
-            ></div>
-            <div class="absolute bottom-0 pl-1 pb-0.5">
-              <p>Age: {{ persona.age }}</p>
-              <p>Gender: {{ persona.gender }}</p>
-              <p>Location: {{ persona.location }}</p>
-            </div>
-            <img
-              :src="persona.profile_image"
-              alt="Profile Picture"
-              class="object-cover w-full h-52"
-            />
+        <div id="profile-picture" class="persona-info relative">
+          <div
+            class="absolute inset-0 bg-gradient-to-t from-slate-50 to-transparent rounded-2xl"
+          ></div>
+          <div class="absolute bottom-0 pl-1 pb-1">
+            <p>Age: {{ persona.age }}</p>
+            <p>Gender: {{ persona.gender }}</p>
+            <p>Location: {{ persona.location }}</p>
           </div>
+          <img
+            :src="persona.profile_image"
+            alt="Profile Picture"
+            class="object-cover w-full h-56"
+          />
         </div>
-        <div id="motivation">
+        <div id="motivation" class="persona-info">
           <div class="flex flex-col gap-2">
             <h5 class="text-xl">Motivation</h5>
             <p class="italic">{{ persona.motivation }}</p>
           </div>
         </div>
-        <div id="hobbies-and-interests">
+        <div id="hobbies-and-interests" class="persona-info">
           <div class="flex flex-col gap-2">
             <h5 class="text-xl">Hobbies and Interests</h5>
             <div class="flex flex-row flex-wrap gap-2">
@@ -38,7 +36,7 @@
             </div>
           </div>
         </div>
-        <div id="goals">
+        <div id="goals" class="persona-info">
           <div class="flex flex-col gap-2">
             <h5 class="text-xl">Goals</h5>
             <div class="flex flex-row flex-wrap gap-2">
@@ -46,13 +44,13 @@
             </div>
           </div>
         </div>
-        <div id="technical-experience">
+        <div id="technical-experience" class="persona-info">
           <div class="flex flex-col gap-2">
             <h5 class="text-xl">Technical experience</h5>
             <p class="italic">{{ persona.technical_experience }}</p>
           </div>
         </div>
-        <div id="challenges">
+        <div id="challenges" class="persona-info">
           <div class="flex flex-col gap-2">
             <h5 class="text-xl">Challenges</h5>
             <div class="flex flex-row flex-wrap gap-2">
