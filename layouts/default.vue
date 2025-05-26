7750eaeb0a30f6aea94aa611699891d7a3931841
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-05-26 21:09:13 +0200

    Refactor layout structure in default.vue to improve readability

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,16 @@
 <template>
-    <template>
-  <div class="min-h-screen flex flex-col">
-    <header class="flex justify-center py-2 h-28">
-      <nuxt-link to="/" class="flex gap-2 items-center">
-        <img src="/img/logo.webp" alt="Logo" class="size-24" />
-        <div class="font-bold text-3xl -ml-4">Characreate</div>
-      </nuxt-link>
-    </header>
-    <main class="flex-1">
-      <slot />
-    </main>
-    <footer>
-    </footer>
-  </div>
-</template>
+  <template>
+    <div class="min-h-screen flex flex-col">
+      <header class="flex justify-center py-2 h-28">
+        <nuxt-link to="/" class="flex gap-2 items-center">
+          <img src="/img/logo.webp" alt="Logo" class="size-24" />
+          <div class="font-bold text-3xl -ml-4">Characreate</div>
+        </nuxt-link>
+      </header>
+      <main class="flex-1">
+        <slot />
+      </main>
+      <footer></footer>
+    </div>
+  </template>
 </template>
